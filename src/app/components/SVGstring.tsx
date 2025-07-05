9de2f86fce7056a2a9b58f28702bd05f3ad3e467
Muntaha <muntahasayed396@gmail.com>
2025-07-05 23:15:21 +0530

    Added string followUp on x axis

--- src/app/components/SVGstring.tsx
+++ src/app/components/SVGstring.tsx
@@ -5,32 +5,34 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
 const SVGstring = () => {
-  const [iinitialPath, setInitialPath] = useState(200)
+  const [YPath, setYPath] = useState(200)
+  const [Xpath, setXpath] = useState(500)
+
   const pathRef = useRef<SVGPathElement>(null)
 
   // Animate the path's "d" attribute with GSAP when iinitialPath changes
   useGSAP(() => {
     if (pathRef.current) {
       gsap.to(pathRef.current, {
-        attr: { d: `M 10 200 Q 645 ${iinitialPath}, 1270 200` },
+        attr: { d: `M 10 200 Q ${Xpath} ${YPath}, 1270 200` },
         duration: 0.8,
         ease: "elastic.out(1,0.2)"
       })
     }
-  }, [iinitialPath])
+  }, [YPath, Xpath])
 
   return (
     <div>
       <div
         id='string'
-        onMouseEnter={() => setInitialPath(200)}
-        onMouseMove={dets => setInitialPath(dets.clientY)}
-        onMouseLeave={() => setInitialPath(200)}
+        onMouseEnter={() => setYPath(200)}
+        onMouseMove={dets => {setYPath(dets.clientY); setXpath(dets.clientX) }}
+        onMouseLeave={() => setYPath(200)}
       >
         <svg className='bg-gray-800 w-full h-96 flex justify-center items-center'>
           <path
             ref={pathRef}
-            d={`M 10 200 Q 505 ${iinitialPath}, 1000 200`}
+            d={`M 10 200 Q 505 200, 1000 200`}
             stroke="white"
             fill="transparent"
           />
